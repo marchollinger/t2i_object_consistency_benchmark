8ad6821167d830f421c84ba05d1f4d933c5f8d6d
Marc Pascal Hollinger <marcpascal.hollinger@uzh.ch>
2024-10-08 17:42:34 +0200

    compare_results docstr

--- compare_results.py
+++ compare_results.py
@@ -4,116 +4,61 @@ from pathlib import Path
 import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
+from matplotlib.figure import Figure
 
 from run_benchmark import aggregate_object_scores
 
 
-def gpt4_save_figures(data_file, score_name, out_dir):
-    full_df = pd.read_csv(data_file, index_col="prompt_id", delimiter="\t")
-    num_df = full_df.loc[:, full_df.columns != "prompt"]
-    line_plot(num_df, score_name, out_dir)
-    compare_means(num_df, score_name, out_dir)
+def compare_means_grouped(df: pd.DataFrame, score_name: str) -> Figure:
+    """Produces a bar plot to visualize the difference between categories.
 
+    Args:
+        df: The final benchmark results for all models merged into one DataFrame
+            with columns ["prompt_id", "variation", "prompt", "category", "score", "model"].
+        metric: The name of the aggregation metric used to obtain the scores per prompt.
 
-def box_plot(df, score_name, out_path):
-    fig, ax = plt.subplots()
-    sns.boxplot(df, ax=ax)
-    ax.set_ylabel(score_name + "_score")
-    fig.savefig(out_path + f"box_plot_{score_name}.pdf")
-
-
-def compare_var(df, score_name, out_path):
-    fig, ax = plt.subplots()
-    df = df.mean(axis=0)
-    x = list(df.index)
-    sns.barplot(x=x, y=df, ax=ax)
-    ax.set_ylabel(score_name + "_score")
-    fig.savefig(out_path + f"var_plot_{score_name}.pdf")
-
-
-def compare_means(df, score_name, out_path):
-    fig, ax = plt.subplots()
-    df = df.mean(axis=0)
-    x = list(df.index)
-    sns.barplot(x=x, y=df, ax=ax)
-    ax.set_ylabel(score_name + " score")
-    fig.savefig(out_path + f"mean_plot_{score_name}.pdf")
-
-
-def line_plot(df, metric):
-    df = df
-    fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, figsize=(15, 10))
-    # sns.lineplot(df["SD2"], ax=ax)
-    x_axis = range(df.shape[0])
-    for column in df:
-        if "real" in column:
-            sns.lineplot(x=x_axis, y=df[column], label=column, marker="o", ax=ax1)
-        else:
-            sns.lineplot(x=x_axis, y=df[column], label=column, marker="o", ax=ax2)
-    ax1.set_title("real")
-    ax1.set_ylabel(metric + "_score")
-    ax1.legend(loc="upper right")
-    ax2.set_title("simple")
-    ax2.set_xlabel("prompt_id")
-    ax2.set_ylabel("mean " + metric + "_score")
-    return fig
-
-
-def comparative_line_plot(df, metric):
-    df = stack_df(df)
-    n_plots = (df.shape[1] // 2) + 1
-    fig, axes = plt.subplots(nrows=n_plots, ncols=1, figsize=(15, 5 * n_plots))
-    for i, m in enumerate(["SD1-5", "SD2", "SD2-1"]):
-        model_df = df[df["model"] == m]
-        sns.lineplot(
-            model_df, x="prompt_id", y="score", hue="task_type", marker="o", ax=axes[i]
-        )
-        axes[i].set_title(m)
-        axes[i].set_xlabel("")
-        axes[i].set_ylabel(metric + " score")
-    axes[-1].set_xlabel("prompt_id")
-    fig.tight_layout()
-    return fig
-
-
-def compare_means_grouped(df, score_name):
+    Returns:
+        A figure containing the plot.
+    """
     fig, ax = plt.subplots()
     sns.barplot(df, x="model", y="score", hue="category", ax=ax)
     ax.set_ylabel("mean " + score_name + " score")
     return fig
 
 
-def box_means_grouped(df, metric):
+def box_means_grouped(df: pd.DataFrame, metric: str) -> Figure:
+    """Produces a box plot to visualize the difference in mean and variation between categories.
+
+    Args:
+        df: The final benchmark results for all models merged into one DataFrame
+            with columns ["prompt_id", "variation", "prompt", "category", "score", "model"].
+        metric: The name of the aggregation metric used to obtain the scores per prompt.
+
+    Returns:
+        A figure containing the plot.
+    """
     fig, ax = plt.subplots()
-    sns.boxplot(df, x="model", y="score", hue="category", ax=ax, order=["SD2", "SD2.1", "SD3-medium", "DALL-E 3"])
+    sns.boxplot(
+        df,
+        x="model",
+        y="score",
+        hue="category",
+        ax=ax,
+        order=["SD2", "SD2.1", "SD3-medium", "DALL-E 3"],
+    )
     ax.set_ylabel(metric + " score")
     ax.set_xlabel(None)
     return fig
 
 
-def save_figures(data_file, metric, out_dir):
-    full_df = pd.read_csv(data_file, index_col="prompt_id", delimiter="\t")
-    num_df = full_df.loc[:, full_df.columns != "prompt"]
-
-    # line_plot(num_df, "align", out_dir).savefig(out_dir + f"line_{metric}_plot.pdf")
-    compare_means_grouped(num_df, metric).savefig(out_dir + f"bar_{metric}_plot.pdf")
-    box_means_grouped(num_df, metric).savefig(out_dir + f"box_{metric}_plot.pdf")
-    comparative_line_plot(num_df, metric).savefig(out_dir + f"line_{metric}_plot.pdf")
-
-
-def stack_df(df):
-    arrays = [
-        ["SD1-5", "SD1-5", "SD2", "SD2", "SD2-1", "SD2-1"],
-        ["simple", "real"] * 3,
-    ]
-    index = pd.MultiIndex.from_arrays(arrays, names=["model", "task_type"])
-    df.columns = index
-    df = df.stack().stack().to_frame().reset_index()
-    df = df.rename(columns={0: "score"})
-    return df
-
-
 def compare_models(model_output_dir: Path, out_dir: Path, metric="std"):
+    """Produce plots comparing all the model results in the directory.
+
+    Args:
+        model_output_dir: Path to directory containing the model results.
+        out_dir: Directory to save the plots to.
+        metric: Metric to use for aggregating the variation scores. Defaults to "std".
+    """
     model_dirs = [m for m in model_output_dir.glob("./*") if m.is_dir()]
     model_names = list(map(lambda x: x.parts[-1], model_dirs))
 
@@ -137,12 +82,6 @@ def compare_models(model_output_dir: Path, out_dir: Path, metric="std"):
 
 
 def main():
-    # clip_file = "./test_model/out/csv/all_clip.csv"
-    # save_figures(clip_file, "clip", "./")
-    # metrics = ["min", "mean", "std"]
-    # for metric in metrics:
-    #     align_file = f"./test_model/out/real-simple/{metric}_align.csv"
-    #     save_figures(align_file, metric, "./plots/")
 
     compare_models(Path("./out"), Path("./"), metric="std")
 
